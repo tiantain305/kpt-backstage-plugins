18155b3069428a3c60a530398105f47cae9056dc
Chandra Shekhar <chandra.b@samsung.com>
2023-05-15 11:49:01 +0530

    Update packageVariant.ts

--- plugins/cad/src/components/ResourceViewerDialog/components/FirstClassViewers/StructuredMetadata/resources/packageVariant.ts
+++ plugins/cad/src/components/ResourceViewerDialog/components/FirstClassViewers/StructuredMetadata/resources/packageVariant.ts
@@ -1,18 +1,19 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+Copyright 2023 The Nephio Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 import { PackageVariant } from '../../../../../../types/PackageVariant';
 import { Metadata } from '../StructuredMetadata';
@@ -64,12 +65,6 @@ export const getPackageVariantStructuredMetadata = (
   for (const field of objFields) {
     setSpecdata(newMetadata, packageVariant.spec[field], field);
   }
-  // setSpecdata(newMetadata, packageVariant.spec.upstream, "upstream");
-  // setSpecdata(newMetadata, packageVariant.spec.downstream, "downstream");
-  // setSpecdata(newMetadata, packageVariant.spec.labels, "labels");
-  // setSpecdata(newMetadata, packageVariant.spec.packageContext, "packageContext");
-  // setSpecdata(newMetadata, packageVariant.spec.injectors, "injectors");
-  // setSpecdata(newMetadata, packageVariant.spec.pipeline, "pipeline");
 
   for (const thisKey of Object.keys(newMetadata)) {
     const isPrefix = thisKey.includes('.');
