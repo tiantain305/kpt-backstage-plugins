ceb7da0066180e7302235502c9465095d8dd40dd
chander12shekhar <chandra.b@samsung.com>
2023-05-17 14:40:54 +0530

    Comment update
    Comment update

--- plugins/cad/src/components/ResourceViewerDialog/components/FirstClassViewers/StructuredMetadata/resources/packageVariant.ts
+++ plugins/cad/src/components/ResourceViewerDialog/components/FirstClassViewers/StructuredMetadata/resources/packageVariant.ts
@@ -1,19 +1,18 @@
-/*
-Copyright 2023 The Nephio Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+/**
+ * Copyright 2023 The Nephio Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import { KubernetesResource } from '../../../../../../types/KubernetesResource';
 import { PackageVariant } from '../../../../../../types/PackageVariant';
@@ -61,31 +60,37 @@ export const getPackageVariantStructuredMetadata = (
   resource: KubernetesResource,
 ): Metadata => {
   const packageVariant = resource as PackageVariant;
-  
+
   const contextdata: Metadata = {};
-  setContextdata(contextdata, packageVariant.spec.packageContext, "packageContext");
+  setContextdata(
+    contextdata,
+    packageVariant.spec.packageContext,
+    'packageContext',
+  );
 
   const customMetadata: Metadata = {
     variantLabels: getValue(packageVariant.spec.labels),
     variantAnnotations: getValue(packageVariant.spec.annotations),
     upstream: packageVariant.spec.upstream
-      ? `${packageVariant.spec.upstream.repo}/${packageVariant.spec.upstream.package}@${packageVariant.spec.upstream?.revision 
-        ? packageVariant.spec.upstream?.revision
-        : '0'}`
+      ? `${packageVariant.spec.upstream.repo}/${packageVariant.spec.upstream.package}@${packageVariant.spec.upstream?.revision}`
       : '',
     downstream: packageVariant.spec.downstream
-      ? `${packageVariant.spec.downstream.repo}/${packageVariant.spec.downstream.package}@${packageVariant.spec.downstream?.revision 
-        ? packageVariant.spec.downstream?.revision
-        : '0'}`
+      ? `${packageVariant.spec.downstream.repo}/${packageVariant.spec.downstream.package}`
       : '',
     packageContext: '',
-    mutators: packageVariant.spec.pipeline?.mutators?.map(getKptFunctionDescription),
-    validators: packageVariant.spec.pipeline?.validators?.map(getKptFunctionDescription),
+    adoptionPolicy: packageVariant.spec.adoptionPolicy,
+    deletionPolicy: packageVariant.spec.deletionPolicy,
+    mutators: packageVariant.spec.pipeline?.mutators?.map(
+      getKptFunctionDescription,
+    ),
+    validators: packageVariant.spec.pipeline?.validators?.map(
+      getKptFunctionDescription,
+    ),
     injectors: packageVariant.spec.injectors
-    ? `${packageVariant.spec.injectors.group}/${packageVariant.spec.injectors.version}/${packageVariant.spec.injectors.kind}@${packageVariant.spec.injectors?.name}`
-    : '',
+      ? `${packageVariant.spec.injectors.group}/${packageVariant.spec.injectors.version}/${packageVariant.spec.injectors.kind}@${packageVariant.spec.injectors?.name}`
+      : '',
   };
-  
+
   for (const thisKey of Object.keys(contextdata)) {
     const isPrefix = thisKey.includes('/');
     const thisKeyName = isPrefix
@@ -98,9 +103,7 @@ export const getPackageVariantStructuredMetadata = (
 
     const fieldKey = thisKey.slice(thisKeyName.length + 1);
     customMetadata[thisKeyName].push(
-      isPrefix
-        ? `${fieldKey}: ${contextdata[thisKey]}`
-        : contextdata[thisKey],
+      isPrefix ? `${fieldKey}: ${contextdata[thisKey]}` : contextdata[thisKey],
     );
   }
   return customMetadata;
--- plugins/cad/src/types/PackageVariant.ts
+++ plugins/cad/src/types/PackageVariant.ts
@@ -1,40 +1,39 @@
-/*
-Copyright 2023 The Nephio Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+/**
+ * Copyright 2023 The Nephio Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import { KptfilePipeline } from './Kptfile';
 import { KubernetesKeyValueObject } from './KubernetesResource';
 
- export type PackageVariant = {
-   kind: string;
-   apiVersion: string;
-   metadata: PackageVariantMetadata;
-   spec: PackageVariantSpec;
- };
- 
- export type PackageVariantMetadata = {
-   name: string;
-   namespace?: string;
-   labels?: KubernetesKeyValueObject;
-   annotations?: KubernetesKeyValueObject;
- };
- 
- export type PackageVariantSpec = {
-  upstream: PackageVariantStream;
-  downstream: PackageVariantStream;
+export type PackageVariant = {
+  kind: string;
+  apiVersion: string;
+  metadata: PackageVariantMetadata;
+  spec: PackageVariantSpec;
+};
+
+export type PackageVariantMetadata = {
+  name: string;
+  namespace?: string;
+  labels?: KubernetesKeyValueObject;
+  annotations?: KubernetesKeyValueObject;
+};
+
+export type PackageVariantSpec = {
+  upstream: PackageVariantUpstream;
+  downstream: PackageVariantDownstream;
   labels: KubernetesKeyValueObject;
   annotations?: KubernetesKeyValueObject;
   packageContext: PackageVariantPackageContext;
@@ -42,22 +41,27 @@ import { KubernetesKeyValueObject } from './KubernetesResource';
   deletionPolicy?: string;
   injectors?: PackageVariantInjectors;
   pipeline?: KptfilePipeline;
- };
+};
 
-  export type PackageVariantStream = {
-    repo: string;
-    package: string;
-    revision?: string;
-  };
+export type PackageVariantUpstream = {
+  repo: string;
+  package: string;
+  revision?: string;
+};
 
-  export type PackageVariantPackageContext = {
-    data: KubernetesKeyValueObject;
-    removeKeys?: string[];
-  };
+export type PackageVariantDownstream = {
+  repo: string;
+  package: string;
+};
 
-  export type PackageVariantInjectors = {
-    group: string;
-    version: string;
-    kind: string;
-    name: string;
-  }
+export type PackageVariantPackageContext = {
+  data: KubernetesKeyValueObject;
+  removeKeys?: string[];
+};
+
+export type PackageVariantInjectors = {
+  group: string;
+  version: string;
+  kind: string;
+  name: string;
+};
