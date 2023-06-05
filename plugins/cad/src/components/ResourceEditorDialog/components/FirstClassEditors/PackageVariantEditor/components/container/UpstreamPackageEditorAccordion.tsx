af403e26a63da8c464f89632e6a6c81e1cf4f598
chander12shekhar <chandra.b@samsung.com>
2023-06-05 20:28:42 +0530

    Updated UpstreamPackageEditor
    Updated UpstreamPackageEditor

--- plugins/cad/src/components/ResourceEditorDialog/components/FirstClassEditors/PackageVariantEditor/components/container/UpstreamPackageEditorAccordion.tsx
+++ plugins/cad/src/components/ResourceEditorDialog/components/FirstClassEditors/PackageVariantEditor/components/container/UpstreamPackageEditorAccordion.tsx
@@ -14,25 +14,27 @@
  * limitations under the License.
  */
 
-import { SelectItem } from '@backstage/core-components';
+import { Progress, SelectItem } from '@backstage/core-components';
+import { Alert } from '@material-ui/lab';
 import { TextField } from '@material-ui/core';
 import React, { Fragment, useRef, useState, useEffect } from 'react';
-import { KubernetesKeyValueObject } from '../../../../../../../types/KubernetesResource';
+import { useApi } from '@backstage/core-plugin-api';
+import useAsync from 'react-use/lib/useAsync';
+import { configAsDataApiRef } from '../../../../../../../apis';
+import { PackageVariantUpstream } from '../../../../../../../types/PackageVariant';
 import { Repository } from '../../../../../../../types/Repository';
+import { emptyIfUndefined } from '../../../../../../../utils/string';
 import { Select } from '../../../../../../Controls';
 import { EditorAccordion } from '../../../Controls';
 import { AccordionState } from '../../../Controls/EditorAccordion';
-
-type OnUpdatedKeyValueObject = (
-  keyValueObject: KubernetesKeyValueObject,
-) => void;
+import { PackageRevision } from '../../../../../../../types/PackageRevision';
 
 type UpstreamPackageObjectEditorProps = {
   id: string;
   title: string;
   state: AccordionState;
-  keyValueObject: KubernetesKeyValueObject;
-  onUpdatedKeyValueObject: OnUpdatedKeyValueObject;
+  keyValueObject: PackageVariantUpstream;
+  onUpdatedKeyValueObject: (arg0: PackageVariantUpstream) => void;
 };
 
 type InternalKeyValue = {
@@ -45,6 +47,41 @@ type RepositorySelectItem = SelectItem & {
   repository?: Repository;
 };
 
+type PackageRevisionSelectItem = SelectItem & {
+  packageRevision?: PackageRevision;
+};
+
+const mapRepositoryToSelectItem = (
+  repository: Repository,
+): RepositorySelectItem => ({
+  label: repository.metadata.name,
+  value: repository.metadata.name,
+  repository: repository,
+});
+
+const mapPackageRevisionToSelectItem = (
+  packageRevision: PackageRevision,
+): PackageRevisionSelectItem => ({
+  label: packageRevision.spec.packageName,
+  value: packageRevision.metadata.name,
+  packageRevision: packageRevision,
+});
+
+export const getRepositoryData = (allRepo: any[], name: string): Repository => {
+  const repository = allRepo.find(thisRepo => thisRepo.metadata.name === name);
+  return repository;
+};
+
+export const getPackageData = (
+  allPackage: any[],
+  name: string,
+): PackageRevision => {
+  const packages = allPackage.find(
+    packageData => packageData.spec.packageName === name,
+  );
+  return packages;
+};
+
 export const UpstreamPackageEditorAccordion = ({
   id,
   title,
@@ -52,42 +89,69 @@ export const UpstreamPackageEditorAccordion = ({
   keyValueObject,
   onUpdatedKeyValueObject,
 }: UpstreamPackageObjectEditorProps) => {
-  const [repository, setRepository] = useState<string>(
-    keyValueObject.repo || 'none',
-  );
-  const [upstreamPackage, setUpstreamPackage] = useState<string>(
-    keyValueObject.package || 'none',
-  );
-  const [repositorySelectItems, setRepositorySelectItems] = useState<string[]>(
-    [],
-  );
-
-  const [packageSelectItems, setPackageSelectItems] = useState<string[]>([]);
-
+  const api = useApi(configAsDataApiRef);
   const refViewModel = useRef<InternalKeyValue>(keyValueObject);
   const viewModel = refViewModel.current;
+  const repositoryName = viewModel.repo;
+  const packageName = viewModel.package;
+
+  const [repository, setRepository] = useState<Repository>();
+  const [packageRevision, setPackageRevision] = useState<PackageRevision>();
+  const [repositorySelectItems, setRepositorySelectItems] = useState<
+    RepositorySelectItem[]
+  >([]);
+  const [packageRevisionSelectItems, setPackageRevisionSelectItems] = useState<
+    PackageRevisionSelectItem[]
+  >([]);
 
   const keyValueObjectUpdated = (): void => {
     onUpdatedKeyValueObject(viewModel);
   };
 
+  const { loading, error } = useAsync(async (): Promise<void> => {
+    const [{ items: thisAllRepositories }, allPackages] = await Promise.all([
+      api.listRepositories(),
+      api.listPackageRevisions(),
+    ]);
+
+    const thisRepository = repositoryName
+      ? getRepositoryData(thisAllRepositories, repositoryName)
+      : undefined;
+
+    const thisPackageRevision = getPackageData(allPackages, packageName);
+    const targetRepositoryItems = thisAllRepositories.map(
+      mapRepositoryToSelectItem,
+    );
+    const allowPackageRevisions = allPackages.map(
+      mapPackageRevisionToSelectItem,
+    );
+    setRepository(thisRepository);
+    setRepositorySelectItems(targetRepositoryItems);
+    setPackageRevision(thisPackageRevision);
+    setPackageRevisionSelectItems(allowPackageRevisions);
+  }, [api]);
+
+  useEffect(() => {
+    viewModel.repo = repository ? repository?.metadata.name : '';
+    onUpdatedKeyValueObject(viewModel);
+  }, [repository]);
+
+  useEffect(() => {
+    viewModel.package = packageRevision
+      ? packageRevision?.spec.packageName
+      : '';
+    onUpdatedKeyValueObject(viewModel);
+  }, [packageRevision]);
+
+  if (loading) {
+    return <Progress />;
+  } else if (error) {
+    return <Alert severity="error">{error.message}</Alert>;
+  }
+
   const description = `${viewModel.repo ? `${viewModel.repo}/` : ''}${
     viewModel.package ? `${viewModel.package}` : ''
-  }${viewModel.package ? `@${viewModel.package}` : ''}`;
-  useEffect(() => {
-    const allRepoList = [];
-    allRepoList.push({
-      label: `${viewModel.repo}`,
-      value: viewModel.repo,
-    });
-    setRepositorySelectItems(allRepoList);
-    const allPackageList = [];
-    allPackageList.push({
-      label: `${viewModel.package}`,
-      value: viewModel.package,
-    });
-    setPackageSelectItems(allPackageList);
-  }, [keyValueObject]);
+  }${viewModel.revision ? `@${viewModel.revision}` : ''}`;
 
   return (
     <EditorAccordion
@@ -99,17 +163,29 @@ export const UpstreamPackageEditorAccordion = ({
       <Fragment>
         <Select
           label="Upstream Repository"
-          onChange={value => setRepository(value)}
-          selected={repository}
+          onChange={selectedRepositoryName =>
+            setRepository(
+              repositorySelectItems.find(
+                r => r.value === selectedRepositoryName,
+              )?.repository,
+            )
+          }
+          selected={emptyIfUndefined(repository?.metadata.name)}
           items={repositorySelectItems}
         />
 
         <Select
           label="Upstream Package"
-          onChange={value => setUpstreamPackage(value)}
-          selected={upstreamPackage}
-          items={packageSelectItems}
+          onChange={value =>
+            setPackageRevision(
+              packageRevisionSelectItems.find(r => r.value === value)
+                ?.packageRevision,
+            )
+          }
+          selected={emptyIfUndefined(packageRevision?.metadata.name)}
+          items={packageRevisionSelectItems}
         />
+
         <TextField
           key="revision"
           label="Revision"
